352d53d5dc114e11aee77851425801f57a99ca72
DmitiyOshchepkov <odo.2003@yandex.ru>
2022-11-11 16:23:42 +0300

    first task

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace Lab5
 {
+    enum task
+    {
+        maxNumber = 1,
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введити задание\n" +
+                              "1) Максимальное число\n" +
+                              "");
+
+            int numTask = -1;
+            while(!int.TryParse(Console.ReadLine(), out numTask) ||
+                numTask <=0 || numTask > 8  || numTask == 7 || numTask == 6)
+            {
+                Console.WriteLine("Неверный ввод задания");
+            };
+            switch(numTask)
+            {
+                case (int)task.maxNumber:
+                    {
+                        Console.WriteLine("Введите количество цифр числа");
+                        int count = -1;
+                        while (!int.TryParse(Console.ReadLine(), out numTask) || numTask < 0 )
+                            Console.WriteLine("Неверный ввод количества цифр");
+
+                        int[] number = new int[count];
+                        int[] digits = new int[10];
+                        for (int i = 0; i < 10; i++)
+                            number[i] = 0;
+
+                        foreach (int digit in number)
+                            digits[digit]++;
+
+                        int sup = 0;
+                        for(int digit = digits.Length - 1; digit >= 0; digit--)
+                        {
+                            for (int j = 0; j < digits[digit]; j++)
+                                number[j + sup] = digit;
+                            sup += digits[digit];
+                        }
+                        Console.WriteLine("Максимальное число из цифр: ");
+                        Console.WriteLine(number);
+                        break;
+                    }
+            }
+            Console.ReadKey();
         }
     }
 }
