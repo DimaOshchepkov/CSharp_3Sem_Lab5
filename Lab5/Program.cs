887d9230d9dc59ecf875136fa717451e422f948f
DmitiyOshchepkov <odo.2003@yandex.ru>
2022-11-16 20:43:51 +0300

    6 ex

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,8 @@ namespace Lab5
         automorphic,
         arifmeticProgression,
         twinsNumber,
+        alphabet,
+        exit = 8,
     }
     internal class Program
     {
@@ -95,126 +98,181 @@ namespace Lab5
             Console.WriteLine("Нельзя представить в виде послдедовательности натуральных чисел");
         }
 
+        public static void PrintAlphabet(int n, int cur = 0, in String alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        {
+            for (int i = 0; i < cur; i++)
+                Console.Write(" ");
+
+            for (int i = cur; i < (26 - cur); i++)
+                Console.Write(alf[i]);
+
+            Console.WriteLine();
+
+            if (26 - cur*2 > n)
+                PrintAlphabet(n, cur + 1, alf);
+
+            if (26 - cur * 2 != n)
+            {
+                for (int i = 0; i < cur; i++)
+                    Console.Write(" ");
+
+                for (int i = cur; i < 26 - cur; i++)
+                    Console.Write(alf[i]);
+
+                Console.WriteLine();
+            }
+        }
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введити задание\n" +
-                              "1) Максимальное число\n" +
-                              "2) Дружественные числа\n" +
-                              "3) Автоморфные числа\n" + 
-                              "4) Арифметическая прогрессия\n" +
-                              "5) Числа близнецы");
-
-            int numTask = -1;
-            while(!int.TryParse(Console.ReadLine(), out numTask) ||
-                numTask <=0 || numTask > 8  || numTask == 7 || numTask == 6)
-            { 
-                Console.WriteLine("Неверный ввод задания");
-            }
-            switch(numTask)
+            while (true)
             {
-                case (int)task.maxNumber:
-                    {
-                        Console.WriteLine("Введите число");
 
-                        int[] number = new int[0];
-                        while (number.Length == 0)
+                Console.WriteLine("Введите задание\n" +
+                                  "1) Максимальное число\n" +
+                                  "2) Дружественные числа\n" +
+                                  "3) Автоморфные числа\n" +
+                                  "4) Арифметическая прогрессия\n" +
+                                  "5) Числа близнецы\n" +
+                                  "6) Алфавит\n" +
+                                  "8) Закрыть");
+
+                int numTask = -1;
+                while (!int.TryParse(Console.ReadLine(), out numTask) ||
+                    numTask <= 0 || numTask > 8 || numTask == 7)
+                {
+                    Console.WriteLine("Неверный ввод задания");
+                }
+                switch (numTask)
+                {
+                    case (int)task.maxNumber:
                         {
-                            
-                            String num = Console.ReadLine();
-                            number = GetNumber(num);
+                            Console.WriteLine("Введите число");
+
+                            int[] number = new int[0];
+                            while (number.Length == 0)
+                            {
+
+                                String num = Console.ReadLine();
+                                number = GetNumber(num);
+                            }
+
+                            int[] digits = new int[10];
+                            for (int i = 0; i < 10; i++)
+                                digits[i] = 0;
+
+                            foreach (int digit in number)
+                                digits[digit]++;
+
+                            int sup = 0;
+                            for (int digit = digits.Length - 1; digit >= 0; digit--)
+                            {
+                                for (int j = 0; j < digits[digit]; j++)
+                                    number[j + sup] = digit;
+                                sup += digits[digit];
+                            }
+                            Console.WriteLine("Максимальное число из цифр: ");
+                            PrintValue(number);
+                            break;
                         }
 
-                        int[] digits = new int[10];
-                        for (int i = 0; i < 10; i++)
-                            digits[i] = 0;
-
-                        foreach (int digit in number)
-                            digits[digit]++;
-
-                        int sup = 0;
-                        for(int digit = digits.Length - 1; digit >= 0; digit--)
+                    case (int)task.friendNumbers:
                         {
-                            for (int j = 0; j < digits[digit]; j++)
-                                number[j + sup] = digit;
-                            sup += digits[digit];
+                            Console.WriteLine("Введите верхнюю границу");
+                            int upperBound = -1;
+                            while (!int.TryParse(Console.ReadLine(), out upperBound) || upperBound <= 0)
+                                Console.WriteLine("Неверный ввод числа");
+
+                            for (int i = 2; i < upperBound; i++)
+                            {
+                                int purposeFriend = SumDivider(i) - i;
+                                if (SumDivider(purposeFriend) - purposeFriend == i &&
+                                    i < purposeFriend && purposeFriend <= upperBound)
+                                    Console.WriteLine(i + " / " + purposeFriend);
+                            }
+
+                            break;
                         }
-                        Console.WriteLine("Максимальное число из цифр: ");
-                        PrintValue(number);
-                        break;
-                    }
-
-                case (int)task.friendNumbers:
-                    {
-                        Console.WriteLine("Введите верхнюю границу");
-                        int upperBound = -1;
-                        while (!int.TryParse(Console.ReadLine(), out upperBound))
-                            Console.WriteLine("Неверный ввод числа");
-
-                        for(int i = 2; i < upperBound; i++)
+                    case (int)task.automorphic:
                         {
-                            int purposeFriend = SumDivider(i) - i;
-                            if (SumDivider(purposeFriend) - purposeFriend == i &&
-                                i < purposeFriend && purposeFriend <= upperBound)
-                                Console.WriteLine(i + " / " + purposeFriend);
+                            Console.WriteLine("Введите верхнюю границу");
+                            int upperBound = -1;
+                            while (!int.TryParse(Console.ReadLine(), out upperBound) || upperBound <= 0)
+                                Console.WriteLine("Неверный ввод числа");
+
+                            Console.WriteLine("Автоморфные числа:");
+                            int num = 1;
+                            int[] sup = new int[3] { 4, 1, 5 };
+                            int s = 0;
+                            while (num <= upperBound)
+                            {
+                                int digitCount = (int)Math.Log10(num) + 1;
+                                if (Math.Pow(num, 2) % (int)Math.Pow(10, digitCount) == num)
+                                    Console.WriteLine(num);
+                                num += sup[s];
+                                s = (s + 1) % 3;
+                            }
+                            break;
                         }
-
-                        break;
-                    }
-                case (int)task.automorphic:
-                    {
-                        Console.WriteLine("Введите верхнюю границу");
-                        int upperBound = -1;
-                        while (!int.TryParse(Console.ReadLine(), out upperBound))
-                            Console.WriteLine("Неверный ввод числа");
-
-                        Console.WriteLine("Автоморфные числа:");
-                        int num = 1;
-                        int[] sup = new int[3] { 4, 1, 5 };
-                        int s = 0;
-                        while(num <= upperBound)
+                    case (int)task.arifmeticProgression:
                         {
-                            int digitCount = (int)Math.Log10(num) + 1;
-                            if (Math.Pow(num, 2) % (int)Math.Pow(10, digitCount) == num)
-                                Console.WriteLine(num);
-                            num += sup[s];
-                            s = (s + 1) % 3;
+                            Console.WriteLine("Введите A");
+                            int a = -1;
+                            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                                Console.WriteLine("Неверный ввод числа");
+
+                            PrintArifmProgr(a);
+
+                            break;
                         }
-                        break;
-                    }
-                case (int)task.arifmeticProgression:
-                    {
-                        Console.WriteLine("Введите A");
-                        int a = -1;
-                        while (!int.TryParse(Console.ReadLine(), out a))
-                            Console.WriteLine("Неверный ввод числа");
 
-                        PrintArifmProgr(a);
+                    case (int)task.twinsNumber:
+                        {
+                            Console.WriteLine("Введите число");
+                            int a = -1;
+                            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                                Console.WriteLine("Неверный ввод числа");
 
-                        break;
-                    }
+                            Console.WriteLine("Числа близнецы:");
 
-                case (int)task.twinsNumber:
-                    {
-                        Console.WriteLine("Введите число");
-                        int a = -1;
-                        while (!int.TryParse(Console.ReadLine(), out a))
-                            Console.WriteLine("Неверный ввод числа");
+                            int[] simplesNum = Eratosphen(2 * a);
+                            int indA = Array.FindIndex(simplesNum, (x => x >= a));
+                            for (int i = indA + 1; i <= 2 * a; i++)
+                                if (simplesNum[i] - simplesNum[i - 1] == 2)
+                                    Console.WriteLine(simplesNum[i] + " " + simplesNum[i - 1]);
 
-                        Console.WriteLine("Числа близнецы:");
+                            break;
+                        }
+                    case (int)task.alphabet:
+                        {
+                            Console.WriteLine("Введите число");
+                            int a = -1;
+                            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0 || a > 26)
+                                Console.WriteLine("Неверный ввод числа");
 
-                        int[] simplesNum = Eratosphen(2*a);
-                        int indA = Array.FindIndex(simplesNum, (x => x >= a));
-                        for (int i = indA + 1; i <= 2 * a; i++)
-                            if (simplesNum[i] - simplesNum[i - 1] == 2)
-                                Console.WriteLine(simplesNum[i] + " " + simplesNum[i - 1]);
+                            PrintAlphabet(a);
 
-                        break;
-                    }
+                            break;
+                        }
+                    case (int)task.exit:
+                        {
+                            return;
+                        }
 
-                    
+
+                }
+
+                Console.WriteLine("Продолжить?(да/нет)");
+                String ans = Console.ReadLine();
+                while (ans != "да" && ans != "нет")
+                    Console.WriteLine("Не понял команду");
+
+                if (ans == "нет")
+                    return;
+
+                Console.Clear();
             }
-            Console.ReadKey();
         }
     }
 }
